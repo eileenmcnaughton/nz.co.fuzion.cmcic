8773b79ce3a306f1baaaf1db0611141373d94f29
Jonathan Dahan <jonathan@dhn.one>
2026-07-27 16:48:31 +0200

    Map CiviCRM billing data to DSP2 context

--- CRM/Core/Payment/CmcicOrderContext.php
+++ CRM/Core/Payment/CmcicOrderContext.php
@@ -37,4 +37,43 @@ class CRM_Core_Payment_CmcicOrderContext {
     return base64_encode(json_encode(array('billing' => $contextBilling), JSON_UNESCAPED_UNICODE));
   }
 
+  /**
+   * Build the order context from CiviCRM payment parameters.
+   *
+   * @param array $params
+   *
+   * @return string
+   */
+  public static function buildFromPaymentParams($params) {
+    $billing = array();
+    $map = array(
+      'firstName' => array('firstName', 'first_name'),
+      'lastName' => array('lastName', 'last_name'),
+      'addressLine1' => array('billingStreetAddress', 'street_address'),
+      'addressLine2' => array('billingSupplementalAddress1'),
+      'addressLine3' => array('billingSupplementalAddress2'),
+      'city' => array('billingCity', 'city'),
+      'postalCode' => array('billingPostalCode', 'postal_code'),
+      'email' => array('email', 'email-Primary', 'email-5'),
+      'phone' => array('phone'),
+    );
+    foreach ($map as $target => $sources) {
+      foreach ($sources as $source) {
+        if (isset($params[$source]) && $params[$source] !== '') {
+          $billing[$target] = $params[$source];
+          break;
+        }
+      }
+    }
+
+    if (!empty($params['billingCountry'])) {
+      $country = $params['billingCountry'];
+      $billing['country'] = preg_match('/^[A-Za-z]{2}$/', $country)
+        ? strtoupper($country)
+        : CRM_Core_PseudoConstant::countryIsoCode($country);
+    }
+
+    return self::build($billing);
+  }
+
 }
